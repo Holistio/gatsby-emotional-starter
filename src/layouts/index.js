01ba0031292eef4a78f12558471c443cad610393
Cserei Zoltán <z@holist.io>
2017-09-26 19:38:24 +0200

    helmet for safety

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 const TemplateWrapper = ({ children }) => {
   <div>
+    <Helmet
+      title="Emotional Gatsby Starter"
+    />
     {children()}
   </div>
 }
