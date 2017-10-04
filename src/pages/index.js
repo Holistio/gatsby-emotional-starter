e4226b835be1be3984db28ea08ca0191f53e5a64
Cserei Zoltán <z@holist.io>
2017-10-04 18:44:32 +0300

    0.1.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,38 @@ import React from 'react'
 import styled from 'react-emotion'
 
 const GreetBox = styled('div')`
-  box-shadow: 0px 0px 32px 4px rgba(220,130,130,0.55);
+  box-shadow: 0px 0px 16px 4px ${props => props.theme.colors.concrete_half};
   padding: 2em;
-  background-color: lightcoral;
+  margin: 1em;
+  background-color: ${props => props.theme.colors.concrete};
 `
-const PinkHeader = styled('h1')`
-  color: pink;
+const Header = styled('h1')`
+  color: ${props => props.theme.colors.blue};
+  font-size: 2em;
 `
-const BlueDescription = styled('p')`
-  color: navy;
+const Tagline = styled('h3')`
+  color: ${props => props.theme.colors.blue};
+  font-weight: 300;
+`
+
+const Paragraph = styled('p')`
+  color: ${props => props.theme.colors.mine_shaft};
   margin: 0;
 `
 
+const Link = styled('a')`
+  color: ${props => props.theme.colors.spring_green};
+  text-decoration: none;
+
+  &:hover {
+    color: ${props => props.theme.colors.blue};
+  }
+`
 const IndexPage = () => (
   <GreetBox>
-    <PinkHeader>Emotional hello!</PinkHeader>
-    <BlueDescription>Please don't cry just yet.</BlueDescription>
+    <Header>emotional hello!</Header>
+    <Tagline>(please don't cry just yet)</Tagline>
+    <Paragraph>Learn more about <Link href="//www.gatsbyjs.org/">Gatsby</Link>, <Link href="//emotion.sh/">emotion</Link> or <Link href="//holist.io">our company</Link>.</Paragraph>
   </GreetBox>
 )
 
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,38 +1,7 @@
-import ReactDOM from 'react-dom/server'
 import React from 'react'
 import Typography from 'typography'
-import { GoogleFont } from 'react-typography'
+import stardustTheme from 'typography-theme-stardust'
 
-// TODO: update to stardust theme
-const options = {
-  baseFontSize: '20px',
-  baseLineHeight: 1.60,
-  googleFonts: [
-    {
-      name: 'Merriweather',
-      styles: ['300', '300i', '400']
-    },
-    {
-      name: 'Josefin Sans',
-      styles: ['400', '600']
-    }
-  ],
-  headerFontFamily: ['Josefin Sans', 'sans-serif'],
-  bodyFontFamily: ['Merriweather', 'serif'],
-  scaleRatio: 2.40
-}
-
-const typography = new Typography(options)
-
-// if (process.env.NODE_ENV !== 'production') {
-//   typography.injectStyles()
-//   if (typeof document !== undefined) {
-//     const googleFonts = ReactDOM.renderToStaticMarkup(
-//       React.createFactory(GoogleFont)({ typography })
-//     )
-//     const head = document.getElementsByTagName('head')[0]
-//     head.insertAdjacentHTML('beforeend', googleFonts)
-//   }
-// }
+const typography = new Typography(stardustTheme)
 
 export default typography
